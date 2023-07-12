aa9843b1ef02ba570ef467bc4b6b5eb1ab504576
Mercy2525 <mercywmuriithi.mm@gmail.com>
2023-07-12 22:49:17 +0300

    Scope lab done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName= "bob";
+
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+  return customerName
+}
+
+
+function setBestCustomer(){
+    bestCustomer= 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+  return bestCustomer
+}
+
+
+const leastFavouriteCustomer = "Mary"
+
+function changeLeastFavoriteCustomer () {
+  leastFavouriteCustomer="Mercy"
+  return leastFavouriteCustomer
+}
